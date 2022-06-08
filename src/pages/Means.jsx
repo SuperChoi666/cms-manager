edb77e910bb9cf0da9d7025f765259c8c2411eea
Choi Super <550823026@qq.com>
2022-06-08 20:11:09 +0800

    '图片上传成功'

--- src/pages/Means.jsx
+++ src/pages/Means.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect ,useState} from "react";
 import "./less/Means.less";
-import { Button, Form, Input, message } from "antd";
+import { Button, Form, Input, message,Upload } from "antd";
 import { GetUserDataApi,ChangeUserDataApi } from "../request/api";
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+
 // const onFinish = (values) => {
 //   console.log("Success:", values);
 // };
@@ -10,7 +12,36 @@ import { GetUserDataApi,ChangeUserDataApi } from "../request/api";
 //   console.log("Failed:", errorInfo);
 // };
 
+// 将图片路径转base64
+const getBase64 = (img, callback) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+  };
+
+// 限制图片大小只能时200kb
+function beforeUpload (file) {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+  
+    if (!isJpgOrPng) {
+      message.error('You can only upload JPG/PNG file!');
+    }
+  
+    const isLt2M = file.size / 1024 / 1024 / 1024  < 200;
+    // 除于三次1024得到（KB）然后再与200（KB）比较
+  
+    if (!isLt2M) {
+      message.error('请上传小于200kB的图!');
+    }
+  
+    return isJpgOrPng && isLt2M;
+  };
+
+
 export default function Means() {
+    const [loading,setLoading] =useState(false)
+  const [imageUrl, setImageUrl] = useState("");
+
   //   const [username1, setUsername1] = React.useState("");
   //   const [password1, setPassword1] = React.useState("");
   // 查询
@@ -45,7 +76,39 @@ export default function Means() {
       })
     }
   }
-
+//   点击了上传图片
+  const handleChange = (info) => {
+    if (info.file.status === 'uploading') {
+      setLoading(true);
+      return;
+    }
+//上传成功
+    if (info.file.status === 'done') {
+        message.success("图片上传成功")
+        // 文件的名称
+        // console .log(info.file.response.data.filePath)
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, (url) => {
+        setLoading(false);
+        setImageUrl(url);
+        // 存储图片名称
+        localStorage.setItem('avatar',info.file.response.data.filePath)
+      });
+    }
+  };
+//   上传按钮
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
+      </div>
+    </div>
+  );
   return (
     <div className="means">
       <Form
@@ -90,6 +153,34 @@ export default function Means() {
           </Button>
         </Form.Item>
       </Form>
+      <p>点击下方修改头像：</p>
+      <Upload
+    //   跟后端要协约好的name字段
+      name="avatar"
+      listType="picture-card"
+      className="avatar-uploader"
+    //   多图上传与单图上传
+      showUploadList={false}
+    //   后端的接口
+      action="/api/upload"
+    //   上传前
+      beforeUpload={beforeUpload}
+      onChange={handleChange}
+      headers={{"cms-token":localStorage.getItem('cms-token')}}
+    >
+      {imageUrl ? (
+        <img
+          src={imageUrl}
+          alt="avatar"
+          style={{
+            width: '100%',
+          }}
+        />
+      ) : (
+        uploadButton
+      )}
+    </Upload>
+
     </div>
   );
 }
